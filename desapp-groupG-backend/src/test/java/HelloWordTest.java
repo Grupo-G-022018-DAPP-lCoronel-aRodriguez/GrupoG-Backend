fa747dc550f5b8591c9d3b1acd445996acc4085f
Luis Coronel <lucho_9292@live.com>
2018-09-03 02:46:11 -0300

    Added class and test

---
+++ desapp-groupG-backend/src/test/java/HelloWordTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class HelloWordTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
