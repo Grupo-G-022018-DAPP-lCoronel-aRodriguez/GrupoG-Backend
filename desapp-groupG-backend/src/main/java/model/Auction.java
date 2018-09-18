f172b08248065a58ba1a01d8dd142bc1eb4477f2
Luis Coronel <lucho_9292@live.com>
2018-09-17 22:37:04 -0300

    Auction sabe aceptar ofertas

--- desapp-groupG-backend/src/main/java/model/Auction.java
+++ desapp-groupG-backend/src/main/java/model/Auction.java
@@ -1,19 +1,22 @@
 package model;
 
+import com.sun.istack.internal.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 public class Auction {
-	private String title;
-	private String description;
+	@NotNull private String title;
+    @NotNull private String description;
 	private String address;
 	private String photoURL;
-	private Integer initialPrice;
+    @NotNull private Integer initialPrice;
+	private Integer currentPrice;
 
-    private LocalDate publicationDate;
-	private LocalDate untilDate;
-	private LocalTime untilTime;
+    @NotNull private LocalDate publicationDate;
+    @NotNull private LocalDate untilDate;
+    @NotNull private LocalTime untilTime;
 
 	private State state;
 	private String ownerEmail;
@@ -24,6 +27,7 @@ public class Auction {
         this.title = title;
         this.description = description;
         this.initialPrice = price;
+        this.currentPrice = price; //comienza siendo igual que el valor inicial
 
         this.publicationDate = start;
         this.untilDate = end;
@@ -76,4 +80,20 @@ public class Auction {
 	public void setOwnerEmail(String ownerEmail) {
 		this.ownerEmail = ownerEmail;
 	}
+
+    public void setCurrentPrice(Integer currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public Integer getCurrentPrice() {
+        return currentPrice;
+    }
+
+	//Methods
+
+    public void acceptBid() {
+        //TODO chequear estado InProgress
+        this.setCurrentPrice((int) (getInitialPrice() + (getInitialPrice() * 0.5)));
+    }
+
 }
