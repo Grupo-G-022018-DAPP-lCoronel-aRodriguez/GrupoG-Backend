49a6648dc97e70f1d2caca0b1492e5a5ed561a2a
Luis Coronel <lucho_9292@live.com>
2018-09-17 22:37:38 -0300

    pom.xml usa java 8; removed unused imports

--- desapp-groupG-backend/src/test/java/ValidatorTest.java
+++ desapp-groupG-backend/src/test/java/ValidatorTest.java
@@ -1,12 +1,9 @@
 
 import model.Validator;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
 
 public class ValidatorTest {
 
