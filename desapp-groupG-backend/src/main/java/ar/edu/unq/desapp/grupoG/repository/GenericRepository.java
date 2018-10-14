48f396956b205b7116c966aed072240c0e86a77d
alannrod <alan.nicolas.rodriguez@gmail.com>
2018-10-14 11:29:50 -0300

    generic repository interface and generic repository jpa class done

--- desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/repository/GenericRepository.java
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/repository/GenericRepository.java
@@ -1,5 +1,14 @@
 package ar.edu.unq.desapp.grupoG.repository;
 
-public class GenericRepository<T> {
-
+public interface GenericRepository<T>{
+	 
+    T create(T t);
+ 
+    void delete(T t);
+ 
+    T find(T t);
+ 
+    T update(T t);
+     
+    Iterable<T> findAll();
 }
