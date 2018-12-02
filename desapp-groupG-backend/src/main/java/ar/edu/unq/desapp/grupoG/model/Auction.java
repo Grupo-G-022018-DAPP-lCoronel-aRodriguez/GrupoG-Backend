32ccb3ec2e9d5d64b8e471d280f9bd3e2cdd8f9b
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-12-02 15:37:56 -0300

    auction sin errores

--- desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/model/Auction.java
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/model/Auction.java
@@ -2,18 +2,13 @@ package ar.edu.unq.desapp.grupoG.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
-import com.hellokoding.auth.model.User;
 
 @Entity
 @Table(name = "auction")
