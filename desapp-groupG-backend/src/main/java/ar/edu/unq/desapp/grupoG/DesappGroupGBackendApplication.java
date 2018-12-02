271340e4d5f5647ee62cb90cd186f0439650b628
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-12-02 14:57:44 -0300

    extendiendo de SpringBootServletInitializer

--- desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/DesappGroupGBackendApplication.java
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/DesappGroupGBackendApplication.java
@@ -2,11 +2,20 @@ package ar.edu.unq.desapp.grupoG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DesappGroupGBackendApplication {
+public class DesappGroupGBackendApplication extends SpringBootServletInitializer{
+	/*Extending the SpringBootServletInitializer class also allows us to configure our application 
+	 * when it’s run by the servlet container, by overriding the configure() method.*/
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DesappGroupGBackendApplication.class);
+    }
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DesappGroupGBackendApplication.class, args);
 	}
 }
