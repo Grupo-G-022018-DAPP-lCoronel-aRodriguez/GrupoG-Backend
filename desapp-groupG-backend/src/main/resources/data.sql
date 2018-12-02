923ced62f1604551859b71ce4f14f25ad332468e
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-12-02 17:41:48 -0300

    validation properties para el usuario

--- desapp-groupG-backend/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO AUCTION
-VALUES (10000 , 'Persistence St. 666', 777, 'For testing purposes', 777, 'Last Bidder', 'asd@gmail.com', 'url', '1994-02-25', 'An Auction', '1989-07-09', '23:59:59.99');
