d308e1ff840bdc336c20558a1b0371c6f4d745f1
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-12-02 16:21:29 -0300

    details service, revisar granted authorities

---
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package ar.edu.unq.desapp.grupoG.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unq.desapp.grupoG.model.Auction;
+import ar.edu.unq.desapp.grupoG.model.User;
+import ar.edu.unq.desapp.grupoG.repository.UserRepository;
+
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Auction auction : user.getAuctions()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(auction.getName()));//???????
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
