195534a0c084133cdddf6812ae73671d3daa27f8
Luis Coronel <lucho_9292@live.com>
2018-12-02 23:30:20 -0300

    Funcionando operaciones del controller list, create, get, delete

--- desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/AuctionController.java
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/AuctionController.java
@@ -1,21 +1,13 @@
 package ar.edu.unq.desapp.grupoG.controllers;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
-import ar.edu.unq.desapp.grupoG.repository.AuctionRepositoryJPA;
 import ar.edu.unq.desapp.grupoG.services.AuctionService;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.unq.desapp.grupoG.model.Auction;
@@ -24,8 +16,11 @@ import ar.edu.unq.desapp.grupoG.model.Auction;
 
 public class AuctionController {
 
-    @Autowired
     private AuctionService service;
+    
+    AuctionController(AuctionService service) {
+		this.service = service;
+	}
 	
 
     @RequestMapping(value="auctions", method = RequestMethod.GET)
@@ -44,21 +39,17 @@ public class AuctionController {
         return service.findById(id);
     }
 
-
-
     
     @RequestMapping(value="auctions/{id}", method = RequestMethod.PUT)
     public Auction update(@PathVariable Long id, @RequestBody Auction modifiedAuction){        
         return service.update(id, modifiedAuction);
     }
     
-    /*
+    
     @RequestMapping(value="auctions/{id}", method = RequestMethod.DELETE)
     public Auction delete(@PathVariable Long id){
-        Auction existingAuction = auctionRepository.findOne(id);
-        auctionRepository.delete(existingAuction);
-        return existingAuction;
         //returns the deleted Auction
-    }*/
+    	return service.delete(id);
+    }
 
 }
--- desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/services/AuctionService.java
+++ desapp-groupG-backend/src/main/java/ar/edu/unq/desapp/grupoG/services/AuctionService.java
@@ -2,16 +2,20 @@ package ar.edu.unq.desapp.grupoG.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.BeanUtils;
-
+import org.springframework.stereotype.Service;
 import ar.edu.unq.desapp.grupoG.exceptions.InvalidIdException;
 import ar.edu.unq.desapp.grupoG.model.Auction;
 import ar.edu.unq.desapp.grupoG.repository.AuctionRepositoryJPA;
 
+@Service
 public class AuctionService {
 	
 	private AuctionRepositoryJPA auctionRepository;
+	
+	AuctionService(AuctionRepositoryJPA repository) {
+		this.auctionRepository = repository;
+	}
 
 	public List<Auction> findAll() {
 		return auctionRepository.findAll();
@@ -38,13 +42,13 @@ public class AuctionService {
 
 	public Auction delete(Long id) {
 		return auctionRepository.findById(id)
-		.map(auction -> {
-			auctionRepository.delete(auction);
-			return auction;
-		})
-		.orElseThrow(() ->
-			new InvalidIdException()
-		);
+				.map(auction -> {
+					auctionRepository.delete(auction);
+					return auction;
+				})
+				.orElseThrow(() ->
+					new InvalidIdException()
+				);
 	}
 
 }
